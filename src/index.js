df0cd7f6041eb1477bfec6992351b09f973d36c7
unknown <hee.1.63m@gmail.com>
2020-04-28 04:47:57 +0900

    fix build path variable.

--- src/index.js
+++ src/index.js
@@ -32,10 +32,7 @@ app.use(jwtMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
 
 const __dirname = path.resolve();
-const buildDirectory = path.resolve(
-  __dirname,
-  '..\\genielog-frontend\\build\\.',
-);
+const buildDirectory = path.resolve(__dirname, '../genielog-frontend/build');
 app.use(serve(buildDirectory));
 app.use(async (ctx) => {
   if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
